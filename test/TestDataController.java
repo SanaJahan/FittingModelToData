57abb5ae1994f79f446056f13fa4e47cb7a7b603
SanaJahan <sanadgrt@gmail.com>
2019-11-01 20:10:22 -0700

    removed extra test class

--- test/TestDataController.java
+++ test/TestDataController.java
@@ -1,2 +1,23 @@
+import org.junit.Test;
+
+import java.io.IOException;
+
+import mvc.DataController;
+
+import static org.junit.Assert.assertEquals;
+
 public class TestDataController {
+
+  DataController dataController = new DataController();
+
+  @Test
+  public void checkClusterDataPointListSize() throws IOException {
+    assertEquals(4500,dataController.readClusterDataSet().size());
+  }
+
+  @Test
+  public void checkLinearDataPointListSize() throws IOException {
+    assertEquals(1170,dataController.readLineDataSet().size());
+  }
+
 }
